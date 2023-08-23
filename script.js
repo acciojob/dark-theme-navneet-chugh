f2cd6df75a7a2bdc81a740723702434fdaae62c8
navneet-chugh <navneetchugh15@gmail.com>
2023-08-23 22:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your code here
 const appDiv = document.getElementById("app");
 const swapButton = document.getElementById("swap");
 
@@ -9,19 +8,18 @@ function swapTheme() {
   swapButton.classList.toggle("button_day");
   swapButton.classList.toggle("button_night");
 
-  // Additional logic to handle animation delays
   const hasNightClass = appDiv.classList.contains("night");
 
   if (hasNightClass) {
-    // Add a delay before checking the class to allow for animations
-    setTimeout(() => {
-      if (!appDiv.classList.contains("night")) {
-        // If the class was removed, reapply it
-        appDiv.classList.add("night");
-      }
-    }, 1000); // Adjust the delay as needed
+    // Use requestAnimationFrame to synchronize with rendering cycle
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        if (!appDiv.classList.contains("night")) {
+          appDiv.classList.add("night");
+        }
+      }, 1000); // Adjust the delay as needed
+    });
   }
 }
 
 swapButton.addEventListener("click", swapTheme);
-
