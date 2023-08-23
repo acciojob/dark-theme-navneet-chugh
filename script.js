62ac075253730479b7158314e3a16b17c4b962bc
navneet-chugh <navneetchugh15@gmail.com>
2023-08-23 21:41:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const appDiv = document.getElementById("app");
+    const swapButton = document.getElementById("swap");
+
+    function swapTheme() {
+      appDiv.classList.toggle("day");
+      appDiv.classList.toggle("night");
+
+      swapButton.classList.toggle("button_day");
+      swapButton.classList.toggle("button_night");
+    }
+
+    swapButton.addEventListener("click", swapTheme);
 
